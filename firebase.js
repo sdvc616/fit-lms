65b0f47556278049f2cd08206cd8e6cdd7a6802a
sdvc616 <abdullah.sdvc@gmail.com>
2026-05-22 19:41:52 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,36 @@
 // firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDafN_kj2uOS7fQw5tIEQSbarvS17ZIAE",
+
+  apiKey: "AIzaSyDafN_kj2uOS7fQw5tIEQSbarGvS17ZIAE",
+
   authDomain: "fit-lms.firebaseapp.com",
+
   databaseURL: "https://fit-lms-default-rtdb.firebaseio.com",
+
   projectId: "fit-lms",
+
   storageBucket: "fit-lms.firebasestorage.app",
+
   messagingSenderId: "523650703056",
+
   appId: "1:523650703056:web:72845c5a1e2d51e95ec8d5"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
+
+// Export Firebase services
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
