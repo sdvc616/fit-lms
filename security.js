87e93f03e7f547d36f56e1c0224d3583de6b111e
sdvc616 <abdullah.sdvc@gmail.com>
2026-05-23 17:28:33 +0500

    Update security.js

--- security.js
+++ security.js
@@ -1,13 +1,13 @@
-// security.js
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 import { auth } from "./firebase.js";
 
-export function requireLogin(callback) {
-    onAuthStateChanged(auth, (user) => {
-        if (!user) {
-            window.location.href = "login.html";
-        } else {
-            callback(user);
-        }
-    });
-}
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.replace("index.html");
+
+    }
+
+});
